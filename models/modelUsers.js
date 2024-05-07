7265be7c2ac437ca8fd348fb4ad712cc8464bf9d
miermontoto <mier@mier.info>
2024-05-07 18:15:53 +0200

    validación estricta de contraseña

--- models/modelUsers.js
+++ models/modelUsers.js
@@ -4,21 +4,21 @@ const { Messages } = require("../messages.js")
 const createUser = async (email, name, password) => {
 	const result = await query('INSERT INTO users (email, name, password, role) VALUES (?, ?, ?, ?)', [email, name, password, 'user'])
 
-	if (result.errno == 1062) {
-		return Messages.USER_ALREADY_EXISTS
-	} else if (result.errno) {
-		return Messages.INTERNAL_ERROR
-	}
-
+	if (result.errno == 1062) return Messages.USER_ALREADY_EXISTS
+	if (result.errno) return Messages.INTERNAL_ERROR
 	return Messages.USER_CREATED
 }
 
 const checkCredentials = async (email, password) => {
-	const user = await query('SELECT id, email FROM users WHERE email = ? AND password = ?', [email, password])
+	const user = await query('SELECT id, email, password FROM users WHERE email = ? AND password = ?', [email, password])
 
-	if (user.length === 0) {
-		return Messages.INVALID_CREDENTIALS
-	}
+	if (user.length === 0) return Messages.INVALID_CREDENTIALS
+
+	// validación estricta de contraseña
+	// la query de búsqueda en base de datos no es case sensitive, lo que
+	// provoca que no se compruebe de forma correcta la contraseña
+	if (user[0].password !== password) return Messages.INVALID_CREDENTIALS
+	delete user.password // eliminar contraseña plana del objeto user
 
 	return user
 }
